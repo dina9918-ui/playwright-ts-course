0b734b36c8bd2477342b3e41089d2d733bc2f108
unknown <dina9918@gmail.com>
2026-07-08 23:21:10 +0300

    removing redandart line

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -1,10 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('sanity test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="login-container"]').click();
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="login-container"]').click();  await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
